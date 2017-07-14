b4073b4d78414e6906029026d10fdfbe0a80d1b7
macaneiro <macaneiro@g.com>
2017-07-14 15:33:50 -0400

    First commit with : level design and association effects from camera

---
+++ Source/Assets/Scripts/Constant/Constants.cs
@@ -0,0 +1,19 @@
+﻿public static class Constants
+{
+    public enum TagObjectName
+    {
+        ApplicationController
+    }
+
+    public enum AnimatorVarName
+    {
+        Idle,
+        Walker
+    }
+
+    public enum ScenesName
+    {
+
+    }
+}
+
---
+++ Source/Assets/Scripts/Controllers/ApplicationController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ApplicationController : Singleton<ApplicationController>
+{
+    
+    protected override void Awake()
+    {
+        base.Awake();
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        this.gameObject.tag = Constants.TagObjectName.ApplicationController.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Método para cambiar la scene.
+    /// </summary>
+    /// <param name="scenename" type ="Constants.ScenesName"></param>
+    internal void ChangeScene(Constants.ScenesName scenename)
+    {
+        SceneManager.LoadScene(scenename.ToString());
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
